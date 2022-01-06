687576853ba7f984c71177af6759307a85d7f908
xulihang <xulihanghai@163.com>
2022-01-06 10:34:59 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # MRZ-dataset
+
 An image dataset of documents with MRZ 
+
+The dataset contains 35 images from the Internet. The MRZ code is annotated.
+
+The images are put into 2 categories. One is synthetic images from alexpsd.ru. The other is real world images.
+
+The data are stored in the data.json file.
+
+A snippet of the json file:
+
+```json
+{
+    "images": [
+        {
+            "type": "realworld",
+            "filename": "1599px-JTK_Stearne_Passport-563x400.jpg",
+            "mrz": "P<CANSTEARNE<<JOHN<TIMOTHY<KELLY<<<<<<<<<<<<\\nGA302922<0CAN5807020M2409043<<<<<<<<<<<<<<02",
+            "source": "http://canadapassporthelp.ca/2009/11/24/assistance-with-canadian-passport-photos/",
+            "url": "https://canadapassporthelp.ca/wp-content/uploads/2009/11/1599px-JTK_Stearne_Passport-563x400.jpg"
+        }
+    ]
+}
+```
---
+++ convertAndDownload.py
@@ -0,0 +1,42 @@
+import json
+
+import requests
+import openpyxl
+
+
+
+def download():
+    wb = openpyxl.load_workbook("mrz.xlsx")
+    sh = wb.get_sheet_by_name("Sheet1")
+    for row in sh.rows:
+        if row[0].value == "Synthetic":
+            filename = row[1].value
+            url = row[4].value
+            r = requests.get(url) 
+            with open(filename,'wb') as f:
+                f.write(r.content)
+            
+
+def convert():
+    result_dict = {}
+    images = []
+    result_dict["images"] = images
+    wb = openpyxl.load_workbook("mrz.xlsx")
+    sh = wb.get_sheet_by_name("Sheet1")
+    head = True
+    for row in sh.rows:
+        if head:
+            head = False
+            continue
+        image = {}
+        image["type"] = row[0].value.lower()
+        image["filename"] = row[1].value
+        image["mrz"] = row[2].value
+        image["source"] = row[3].value
+        image["url"] = row[4].value
+        images.append(image)
+    with open("data.json",'w') as f:
+        f.write(json.dumps(result_dict,indent=4))
+
+
+convert()
